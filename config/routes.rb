9dedc3b3a8114f3d90b70b989e66bfd0e4f2415c
jd pagano <jonathanpagano@gmail.com>
2014-08-27 00:19:18 -0400

    Add sessions resource to routes for access to create method

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
    get 'login' => 'sessions#new', as: 'login'
    get 'logout' => 'sessions#destroy', as: 'logout'
 
+   resource :sessions
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
